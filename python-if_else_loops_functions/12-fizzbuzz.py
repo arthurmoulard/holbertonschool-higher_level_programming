adcaf2f97b252e571c5019019053e55ce91eeead
arthurmoulard <arthurmoulardperso@gmail.com>
2026-01-15 14:01:22 +0100

    Add Files

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def fizzbuzz():
+    for i in range(0,100):
+        if(i % 3 == 0 and i % 5 == 0):
+            print("FizzBuzz")
+        elif(i % 5 == 0):
+            print("Buzz")
+        elif(i % 3 == 0):
+            print("Fizz")
+        else:
+            print(f"{i}")
+        print(" ")
