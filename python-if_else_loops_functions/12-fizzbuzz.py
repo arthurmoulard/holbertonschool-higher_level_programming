7902af3d1752ff541e8d4987a42410e88cc8cc21
arthurmoulard <arthurmoulardperso@gmail.com>
2026-01-15 14:30:46 +0100

    Fix Files

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,7 +6,7 @@ def fizzbuzz():
         elif(i % 5 == 0):
             print("Buzz", end=" ")
         elif(i % 3 == 0):
-            print("Fizz")
+            print("Fizz", end=" ")
         else:
             print(f"{i}", end=" ")
         print(" ", end=" ")
