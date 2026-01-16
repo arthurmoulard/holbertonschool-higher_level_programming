f004992df0f00eec60c1f108446f96d239664bbe
arthurmoulard <arthurmoulardperso@gmail.com>
2026-01-16 11:46:51 +0100

    Fix Files

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(0, 9):
     for j in range(i + 1, 10):
-        print(f"{i}{j}", end=", " if i != 8 or j != 9 else "\n")
+        if i == 8:
+            print("{:d}{:d}".format(i, j))
+            break
+        print("{:d}{:d}".format(i, j), end=", ")
