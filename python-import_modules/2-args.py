f260aafb394449d7fd49ffd58bca35beb42b7bb8
arthurmoulard <arthurmoulardperso@gmail.com>
2026-01-16 11:05:11 +0100

    Fix Files

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,13 @@
-#!/usr/bin/env python3
-import sys
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
 
-count = len(sys.argv) - 1
-
-if count == 0:
-    print("0 arguments.")
-elif count == 1:
-    print("1 argument:")
-else:
-    print(f"{count} arguments:")
-
-index = 1
-for arg in sys.argv[1:]:
-    print(f"{index}: {arg}")
-    index += 1
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
