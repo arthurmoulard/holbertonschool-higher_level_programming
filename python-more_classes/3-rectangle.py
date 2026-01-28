db3c8dc427d83fde39585e107334adb6171d2ab7
arthurmoulard <arthurmoulardperso@gmail.com>
2026-01-28 14:47:13 +0100

    Fix Files

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     def __init__(self, width=0, height=0):
         """Parameters of the rectangle"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
