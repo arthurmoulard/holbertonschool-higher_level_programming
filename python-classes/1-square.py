476d68e30d1d6f7daf30e47b4be722e33e8b7dd8
arthurmoulard <arthurmoulardperso@gmail.com>
2026-01-28 10:11:48 +0100

    Fix Files Doc

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+"""define square class"""
 class Square:
+    """represent a square"""
     def __init__(self, size):
         self.__size = size
-
--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""define square class"""
 class Square:
     def __init__(self, size=0):
+        """parameters of the square"""
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
