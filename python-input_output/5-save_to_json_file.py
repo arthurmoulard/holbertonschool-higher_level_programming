965fe039d7994b07e8210cb2c741bb55d92a2137
arthurmoulard <arthurmoulardperso@gmail.com>
2026-02-11 11:01:42 +0100

    Add Files

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,5 +4,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     with open("my_obj.json", "w") as filename:
-        json.dump(my_obj, filename)
-        
+        return json.dump(my_obj, filename)
+    
